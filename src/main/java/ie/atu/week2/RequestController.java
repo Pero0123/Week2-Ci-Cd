03af9490aa0f92284552c8c8ec235123aa77657e
G00422221@atu.ie <G00422221@atu.ie>
2024-09-25 15:24:44 +0100

    Created basic @GetMapping

--- src/main/java/ie/atu/week2/RequestController.java
+++ src/main/java/ie/atu/week2/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
